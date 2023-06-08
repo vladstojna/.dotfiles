3ec56ddb4edb69bc6c2eb0f3edcaf25627ff4775
vladstojna <vlad.leic@gmail.com>
2023-06-08 03:57:36 +0100

    remove bspwm configs launching programs
    - migrate to systemd units

--- .config/bspwm/config/programs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-snixembed --fork
-"$HOME"/.config/polybar/launch.sh &
-pgrep -x sxhkd >/dev/null ||
-    sxhkd -c "$HOME"/.config/sxhkd/sxhkdrc "$HOME"/.config/sxhkd/sxhkdrc.* &
-(sleep 3 && nitrogen --restore) &
