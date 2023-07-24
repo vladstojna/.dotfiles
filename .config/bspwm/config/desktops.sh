edd4e5a8679deba6788c026599c2202e5cef8c11
vladstojna <vlad.leic@gmail.com>
2023-07-24 15:35:36 +0100

    bspwm: remove old desktop setters

--- .config/bspwm/config/desktops.sh
+++ .config/bspwm/config/desktops.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-readonly mon_top="DP-0"
-readonly mon_bottom="DP-2"
-
-bspc monitor "$mon_bottom" -d 1 2 3 4 5 6
-bspc monitor "$mon_top" -d X
-bspc monitor -f "$mon_bottom"
+bspc monitor -d 1 2 3 4 5 6 7 8
