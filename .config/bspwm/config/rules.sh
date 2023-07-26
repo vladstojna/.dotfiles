db0b3b1e8cd151dec3470f070bb500184823efca
vladstojna <vlad.leic@gmail.com>
2023-07-26 22:21:53 +0100

    bspwm: add rules

--- .config/bspwm/config/rules.sh
+++ .config/bspwm/config/rules.sh
@@ -8,6 +8,8 @@ bspc rule -a "*:Toolkit:Picture-in-Picture" state=floating sticky=on follow=off 
 bspc rule -a Blueman-manager:blueman-manager state=floating
 bspc rule -a Pavucontrol:pavucontrol state=floating
 bspc rule -a Zathura:org.pwmt.zathura state=tiled
+bspc rule -a "zoom:*" state=floating
+bspc rule -a "spectacle:spectacle" state=floating
 
 bspc rule -a Ardour:ardour_ardour -o follow=off desktop="$(bspwm-scratchpad --name)"
 bspc rule -a thunderbird:Mail -o follow=off desktop="$(bspwm-scratchpad --name)"
